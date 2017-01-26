bd16cbc86de79db5c3e41016c3890cf2b884d9d1
DGVY <DGVYLK@Gmail.com>
2017-01-26 23:15:21 +0800

    添加注册按钮

--- app/views.py
+++ app/views.py
@@ -6,8 +6,16 @@ from SQLite import SQLiteOp
 from flask import request,session
 
 db = SQLiteOp("LabDB.db")
-app.secret_key = 'asjdlkfhoiaghnmn o45'
+app.secret_key = '\xe0\xae?\xd8\x18\x9c\xb3\xf5\x9e/3@h1\xbf2\xbb\x1e\xafBOF\xbf\xb4'
 
+@app.before_request
+def before_action():        # url的预处理
+    print(request.path)
+    if request.path.find('.ico') == -1:
+        if not request.path == '/login':
+            if (not 'id' in session) or (session['id'] == None):
+                session['newurl'] = request.path
+                return redirect(url_for('login'))
 
 @app.route('/')
 @app.route('/index')
@@ -27,11 +35,16 @@ def fetch_name_from_id(id):
 @app.route('/login', methods=['POST','GET'])
 def login():
     error = None
+    print("===================")
     if request.method == 'POST':
+        if request.form['id'] == '':                # 空用户名
+            error = u'用户名或密码错误，请重新输入！'
+            return render_template('login.html', error=error)
         r_password = fetch_password_from_id(request.form['id'])   # 获取学号对应的密码
         if request.form['password']==r_password:    # 若输入密码与获取的密码对应
             session['id'] = request.form['id']      # 用session传递用户名参数
             session['name'] = fetch_name_from_id(request.form['id'])
+            print(session['name'])
             if 'newurl' in session:                 # 判断是否由本网站的其它域名转来
                 newurl = session['newurl']
                 session.pop('newurl',None)
@@ -44,8 +57,4 @@ def login():
 
 @app.route('/home')
 def home():
-    if 'id' in session:                             # 判断是否已经登录
-        return render_template('home.html', id=session['name'])
-    else:
-        sesson['newurl'] = 'home'
-        return redirect(url_for('login'))
+    return render_template('home.html', name=session['name'])
